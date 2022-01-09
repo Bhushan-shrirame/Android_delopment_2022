67333fbba886c9216ac3e588b733f42f954d2970
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-01-09 21:48:44 +0530

    webview app

---
+++ WebView/app/src/main/java/com/bhushanshrirame/webview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.bhushanshrirame.webview;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String websiteURL = "https://www.google.co.in/"; // sets web url
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Webview stuff
+        webview = findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(websiteURL);
+        webview.setWebViewClient(new WebViewClientDemo());
+
+    }
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        //Keep webview in app when clicking links
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
