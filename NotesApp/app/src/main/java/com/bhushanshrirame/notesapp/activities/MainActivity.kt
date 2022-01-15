9381e64be483f24e938d9454f8505515ea1dda4a
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-01-15 20:12:31 +0530

    item layout

--- NotesApp/app/src/main/java/com/bhushanshrirame/notesapp/activities/MainActivity.kt
+++ NotesApp/app/src/main/java/com/bhushanshrirame/notesapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bhushanshrirame.notesapp
+package com.bhushanshrirame.notesapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bhushanshrirame.notesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
