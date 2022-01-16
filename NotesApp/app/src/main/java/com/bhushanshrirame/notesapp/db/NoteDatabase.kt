274b78d6e2cfa83ef0575d77ab21e4411d1cadbe
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-01-16 19:39:02 +0530

    item layout

--- NotesApp/app/src/main/java/com/bhushanshrirame/notesapp/db/NoteDatabase.kt
+++ NotesApp/app/src/main/java/com/bhushanshrirame/notesapp/db/NoteDatabase.kt
@@ -1,4 +1,47 @@
 package com.bhushanshrirame.notesapp.db
 
-class NoteDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.bhushanshrirame.notesapp.model.Note
+
+
+@Database(
+
+    entities = [Note::class],
+    version =  1,
+    exportSchema = false
+)
+
+abstract class NoteDatabase :RoomDatabase(){
+
+    abstract fun getNoteDao(): DAO
+
+    companion object
+    {
+        @Volatile
+        private var instance: NoteDatabase?= null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: createDatabase(context).also {
+                instance=it
+            }
+        }
+
+
+
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            NoteDatabase::class.java,
+            "note_database"
+        ).build()
+
+
+    }
+
+
+
 }
